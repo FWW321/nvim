f6af0242e094c661b4a2ada83e0d7e3bf7dbee64
FWW <3223400498@qq.com>
2025-06-21 03:32:30 +0800

    macos left-option -> alt

--- init.lua
+++ init.lua
@@ -1,3 +1,9 @@
+-- 在MacOS上将左Option键映射为Alt键
+-- 以ghostty为例
+-- macos-option-as-alt = left
+-- 可选（解除终端对alt+left(right)的绑定，这样会直接透传给应用程序（如neovim）处理）
+-- keybind = alt+left=unbind
+-- keybind = alt+right=unbind
 require("core.options")
 require("core.keymaps")
 require("core.lazy")

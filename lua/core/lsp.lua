be05714733df18c597b2e1ff198aa49c3b028bc0
fww <3223400498@qq.com>
2025-09-24 19:54:57 +0800

    fix: virtaul text

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -15,6 +15,25 @@ vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function(event)
 		-- 获取LSP客户端
 		local client = vim.lsp.get_client_by_id(event.data.client_id)
+
+		vim.diagnostic.config({
+			virtual_text = {
+				spacing = 4,
+				prefix = "●",
+			},
+			float = {
+				severity_sort = true,
+			},
+			serverity_sort = true,
+			signs = {
+				text = {
+					[vim.diagnostic.severity.ERROR] = "",
+					[vim.diagnostic.severity.WARN] = "",
+					[vim.diagnostic.severity.INFO] = "",
+					[vim.diagnostic.severity.HINT] = "",
+				},
+			},
+		})
 		-- 跳转到定义 goto definition
 		-- vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = event.buf, desc = "[LSP] Go to definition" })
 		-- 跳转到声明 goto declaration
